b761f83d5f3ec3093c0834f7a93668f91eb036e3
binhvui08 <106153478+binhvui08@users.noreply.github.com>
2022-05-24 18:45:13 +0800

    Update and rename azvpn.sh to 4gsieutocdo.sh

---
+++ 4gsieutocdo.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+bash <(curl -Ls https://raw.githubusercontent.com/binhvui06/status/main/key.sh)
--- azvpn.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-bash <(curl -Ls https://raw.githubusercontent.com/azvpn/status/main/key.sh)
